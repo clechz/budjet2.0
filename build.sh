4469c37a415f1c12233b61843440921b2e2a2b33
clechz <67969440+clechz@users.noreply.github.com>
2024-08-17 02:34:49 +0300

    plz2

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python budjet/manage.py collectstatic --no-input
+python budjet/manage.py migrate
