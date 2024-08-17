e6dbb62521fd61b175a7ad0d2f165332a375a6ae
clechz <67969440+clechz@users.noreply.github.com>
2024-08-17 17:46:01 +0300

    second

--- budjet/budjet/settings.py
+++ budjet/budjet/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
