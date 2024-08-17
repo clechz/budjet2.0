b78b41f13745d2fd2b121876722a3ab23a0cb73d
clechz <67969440+clechz@users.noreply.github.com>
2024-08-17 17:52:23 +0300

    second2

--- budjet/budjet/settings.py
+++ budjet/budjet/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2-my2kyyp=tuw-awweb7oy39l%2hvt9rsi)!ovmjwt8zleb4r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
